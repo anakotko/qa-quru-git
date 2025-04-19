cf26c555a081d2ceb50c6b4db11ef7fa12c678d0
anakotko <165277916+anakotko@users.noreply.github.com>
2025-04-19 18:03:19 +0700

    Update Main.java

--- src/main/java/com/nastya20022020/Main.java
+++ src/main/java/com/nastya20022020/Main.java
@@ -3,10 +3,12 @@ package com.nastya20022020;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static String msg="Hi!!!!!!!!!";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hi!!!!!!!!!");
+        System.out.printf(msg);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
